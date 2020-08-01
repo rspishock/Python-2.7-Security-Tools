abed00e6ef63c3bfb15193ce93e07738ab1951e8
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-08-01 14:24:52 -0400

    Initial commit

---
+++ UDP_Client.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+
+import socket
+
+target_host = '127.0.0.1'
+target_port = 80
+
+# create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# send some data
+client.sendto('AAABBBCCC', (target_host,target_port))
+
+# receive data
+data, addr = client.recvfrom(4096)
+
+print data
+print addr
