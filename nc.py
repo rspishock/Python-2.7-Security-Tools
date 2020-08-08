171a9b873f4018b0a05c593c21717d43fdf0ba7f
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-08-08 17:26:04 -0400

    Update main()

--- nc.py
+++ nc.py
@@ -16,10 +16,11 @@ target = ''
 upload_destination = ''
 port = 0
 
+
 def usage():
     print 'Net Tool'
     print
-    print 'Usage: nc.py -t --target target_host -p --port port'
+    print 'Usage: nc.py -t --target <target_host> -p --port <port>'
     print '-l --listen\t\t\t\t\t\t - listen on [host]:[port] for incoming connections.'
     print '-e --execute=<file_to_execute>\t - execute the given file upon receiving a connection.'
     print '-c --command\t\t\t\t\t - initialize a command shell.'
@@ -33,4 +34,61 @@ def usage():
     print 'echo "ABCDEFGHI" | ./nc.py -t 192.168.11.12 -p 135'
     sys.exit(0)
 
-usage()
+
+def main():
+    global listen
+    global port
+    global execute
+    global command
+    global upload_destination
+    global target
+
+    if not len(sys.argv[1:]):
+        usage()
+
+    # read the commandline options
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hle:t:p:cu', ['help', 'listen', 'execute', 'target', 'port', 'command'
+                                                                , 'upload'])
+    except getopt.GetoptError as err:
+        print(str(err))
+        usage()
+
+    for o, a in opts:
+        if o in ('-h', '--help'):
+            usage()
+        elif o in ('-l', '--listen'):
+            listen = True
+        elif o in ('-e', '--execute'):
+            execute = True
+        elif o in ('-c', '--commandshell'):
+            command = True
+        elif o in ('-u', '--upload'):
+            upload_destination = a
+        elif o in ('-t', '--target'):
+            target = a
+        elif o in ('-p', '--port'):
+            port = int(a)
+        else:
+            assert  False, "Unhandled Option"
+
+
+    # listen or just send data
+    if not listen and len(target) and port > 0:
+        # read in the buffer from the command line
+        # this will block, so send CTRL-D if not sending input
+        # to stdin
+        buffer = sys.stdin.read()
+
+        # send data off
+        client_sender(buffer)
+
+
+    # listen and potentially
+    # upload things, execute commands, and drop a shell back
+    # depending on command line options above
+    if listen:
+        server_loop()
+
+
+main()
