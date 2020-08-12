99ec129ce122cda558969742f11783e6ccef54e0
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-08-12 16:11:26 -0400

    Add server_loop() and run_command() functions

--- nc.py
+++ nc.py
@@ -126,4 +126,39 @@ def client_sender(buffer):
         # tear down connection
         client.close()
 
+
+def server_loop():
+    global target
+
+    # if no target is defined, listen on all interfaces
+    if not len(target):
+        target = '0.0.0.0'
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((target, port))
+
+    server.listen(5)
+
+    while True:
+        client_socket, addr = server.accept()
+
+        # spin off a thread to handle new client
+        client_thread = threading.Thread(target = client_handler, args = (client_socket))
+        client_thread.start()
+
+
+def run_command(command):
+    # trim the new line
+    command = command.rstrip()
+
+    # run the command and get the output back
+    try:
+        output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+    except:
+        output = 'Failed to execute command.\r\n'
+
+    # send output back to client
+    return output
+
+
 main()
