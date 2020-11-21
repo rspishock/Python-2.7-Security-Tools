77f1faf7123e079dce4c5e6373fc7c676599bc01
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-11-21 18:00:14 -0500

    Add hexdump function to TCP_Proxy.py

--- TCP_Proxy.py
+++ TCP_Proxy.py
@@ -70,6 +70,22 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
             print('[==>] Sent to remote.')
 
 
+# this is a pretty hex dumping function taken directoy from
+# the comments here
+# http://code.activestate.com/recipes/142812-hex-dumper
+def hexdump(src, length = 6):
+    result = []
+    digits = 4 if isinstance(src, unicode) else 2
+
+    for i in xrange(0, len(src), length):
+        s = src[i:i + length]
+        hexa = b' '.join(["%0*x" % (digits, ord(x)) for x in s])
+        text - b''.join([x if 0x20 <= ord(x) < 0x7f else b'.' for x in s])
+        result.append(b"%04X %-*s %s" % (i, length*(digits + 1), hexa, text))
+
+        print b'\n'.join(result)
+
+
 def main():
     # no fancy command line parsing
     if len(sys.argv[1:]) != 5:
