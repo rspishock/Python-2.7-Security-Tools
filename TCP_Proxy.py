4b5728c40b1ddaf21ff47da8ce7dc86cafb1a539
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-11-22 08:30:24 -0500

    Correct typo in TCP_Proxy.py

--- TCP_Proxy.py
+++ TCP_Proxy.py
@@ -80,7 +80,7 @@ def hexdump(src, length = 6):
     for i in xrange(0, len(src), length):
         s = src[i:i + length]
         hexa = b' '.join(["%0*x" % (digits, ord(x)) for x in s])
-        text - b''.join([x if 0x20 <= ord(x) < 0x7f else b'.' for x in s])
+        text = b''.join([x if 0x20 <= ord(x) < 0x7f else b'.' for x in s])
         result.append(b"%04X %-*s %s" % (i, length*(digits + 1), hexa, text))
 
         print b'\n'.join(result)
