27872942f9e047a3ca4169ac83d816aaf903b48e
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-11-22 20:32:59 -0500

    Refactor and test code

--- TCP_Proxy.py
+++ TCP_Proxy.py
@@ -7,74 +7,10 @@ import socket
 import sys
 
 
-def server_loop(local_host, local_port, remote_host, remote_port, receive_first):
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    try:
-        server.bind((local_host, local_port))
-    except:
-        print "[!!] Failed to listen on %s%d" % (local_host, local_port)
-        print "[!!] Check for other listening sockets or correct permissions."
-        sys.exit(0)
-
-    print "[*] Listening on %s:%d" % (local_host, local_port)
-
-    server.listen(5)
-
-    while True:
-        client_socket, addr = server.accept()
-
-        # print the local connection information
-        print "[==>] Received incoming connection from %s:%d" % (addr[0], addr[1])
-
-        # start a thread to talk to the remote host
-        proxy_thread = threading.Thread(target=proxy_handler, args=(client_socket, remote_host, remote_port,
-                                                                    receive_first))
-
-        proxy_thread.start()
-
-
-def proxy_handler(client_socket, remote_host, remote_port, receive_first):
-
-    # connect to the remote host
-    remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    remote_socket.connect((remote_host, remote_port))
-
-    # receive data from the remote end if necessary
-    if receive_first:
-        remote_buffer = receive_from(remote_socket)
-        hexdump(remote_buffer)
-
-        # send it to response buffer
-        remote_buffer = response_handler(remote_buffer)
-
-        # if we have data to send to local client, send it
-        if len(remote_buffer):
-            print('[<==] Sending %d bytes to localhost.' % len(remote_buffer))
-            client_socket.send(remote_buffer)
-
-    # no loop and read from local
-    while True:
-
-        # read from local host
-        local_buffer = receive_from(client_socket)
-
-        if len(local_buffer):
-            print('[==>] Received %d bytes from localhost.' % len(local_buffer))
-            hexdump(local_buffer)
-
-            # send to request handler
-            local_buffer = request_handler(local_buffer)
-
-            # send data to remote host
-            remote_socket.send(local_buffer)
-            print('[==>] Sent to remote.')
-
-
 # this is a pretty hex dumping function taken directly from
 # the comments here
 # http://code.activestate.com/recipes/142812-hex-dumper
-def hexdump(src, length = 6):
+def hexdump(src, length=16):
     result = []
     digits = 4 if isinstance(src, unicode) else 2
 
@@ -121,6 +57,70 @@ def request_handler(buffer):
 def response_handler(buffer):
     # perform packet modifications
     return buffer
+
+
+def proxy_handler(client_socket, remote_host, remote_port, receive_first):
+
+    # connect to the remote host
+    remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remote_socket.connect((remote_host, remote_port))
+
+    # receive data from the remote end if necessary
+    if receive_first:
+        remote_buffer = receive_from(remote_socket)
+        hexdump(remote_buffer)
+
+        # send it to response buffer
+        remote_buffer = response_handler(remote_buffer)
+
+        # if we have data to send to local client, send it
+        if len(remote_buffer):
+            print('[<==] Sending %d bytes to localhost.' % len(remote_buffer))
+            client_socket.send(remote_buffer)
+
+    # no loop and read from local
+    while True:
+
+        # read from local host
+        local_buffer = receive_from(client_socket)
+
+        if len(local_buffer):
+            print('[==>] Received %d bytes from localhost.' % len(local_buffer))
+            hexdump(local_buffer)
+
+            # send to request handler
+            local_buffer = request_handler(local_buffer)
+
+            # send data to remote host
+            remote_socket.send(local_buffer)
+            print('[==>] Sent to remote.')
+
+
+def server_loop(local_host, local_port, remote_host, remote_port, receive_first):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        server.bind((local_host, local_port))
+    except:
+        print "[!!] Failed to listen on %s:%d" % (local_host, int(local_port))
+        print "[!!] Check for other listening sockets or correct permissions."
+        sys.exit(0)
+
+    print "[*] Listening on %s:%d" % (local_host, local_port)
+
+    server.listen(5)
+
+    while True:
+        client_socket, addr = server.accept()
+
+        # print the local connection information
+        print "[==>] Received incoming connection from %s:%d" % (addr[0], addr[1])
+
+        # start a thread to talk to the remote host
+        proxy_thread = threading.Thread(target=proxy_handler, args=(client_socket, remote_host, remote_port,
+                                                                    receive_first))
+
+        proxy_thread.start()
 
 
 def main():
