219019ed63559388b427c42789221e4244f9dfe0
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-11-24 17:59:30 -0500

    Add ssh_command function

--- bh_sshcmd.py
+++ bh_sshcmd.py
@@ -1,7 +1,22 @@
 #! /usr/bin/python
-""
+"Creates a connection to an SSH server and runs a single command."
 
 import subprocess
 import threading
 import paramiko
 
+
+def ssh_command(ip, user, passwd, command):
+    client = paramiko.SSHClient()
+    # client.load host_keys('/home/ryan/.ssh/known_hosts')
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, username=user, password=passwd)
+    ssh_session = client.get_transport().open_session()
+
+    if ssh_session.active:
+        ssh_session.exec_command(command)
+        print ssh_session.recv(1024)
+    return
+
+
+ssh_command('192.168.100.131', 'ryan', 'password', 'id')
